b5170e74531ba08e2ea431d5930bec2ee269a7e0
4701256-GH <4701256@spsd.sk.ca>
2025-11-24 09:53:47 -0600

    what the project is

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
-// Project Title
-// Your Name
-// Date
+// Spex - The digtle life asistent living on your computer
+// Treyden Lee Styles 
+// 11/24/2025 
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
